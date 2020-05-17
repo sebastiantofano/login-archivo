d2c496f6993ad62363c8f6e91c911381c5da522d
Sebastian <sebastiantofano@gmail.com>
2020-05-16 22:57:28 -0300

    Mejora

--- cambiar-contraseña.php
+++ cambiar-contraseña.php
@@ -9,21 +9,23 @@ if(!isset($_SESSION["usuario"])){
 }
 $usuario = $_SESSION["usuario"];
 $archivo = $usuario . ".json";
+$dir = __DIR__ . "/usuarios/";
 
-$clave = $_POST["clave-nueva"];
+$claveNueva = $_POST["clave-nueva"];
 
 //    Convierte el archivo en un string (pero tiene formato JSON)
-$archivoUsuarioJSON = file_get_contents($archivo);
+$archivoUsuarioJSON = file_get_contents($dir.$archivo);
 //Convierte un string codificado en JSON a una variable de PHP
 $archivoUsuarioArray = json_decode($archivoUsuarioJSON,TRUE);
 
 //Pongo en el array la nueva contraseña
-$archivoUsuarioArray["clave"] = md5($clave);
+$archivoUsuarioArray["clave"] = md5($claveNueva);
 //Convierto a JSON y escribo en el archivo
-file_put_contents($archivo, json_encode($archivoUsuarioArray));
+file_put_contents($dir.$archivo, json_encode($archivoUsuarioArray));
 
 header("Location: interno.php?cambio=true");
 exit();
+
 
 ?>
 
